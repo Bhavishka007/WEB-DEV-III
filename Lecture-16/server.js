986b84bc529052f8f532b5c3bda3b542fbe0389a
Bhavishka <bhavishkajangid@gmail.com>
2026-09-18 10:54:10 +0530

    lecture 16

---
+++ Lecture-16/server.js
@@ -0,0 +1,49 @@
+const express=require("express");
+const morgan=require("morgan");
+const app=express();
+const PORT=3000
+
+app.use(morgan());
+
+// const logMiddleware=(req,res,next)=>{
+//     //console.log(req.name)
+//     req.name="John Doe"; 
+//     console.log(`Method:${req.method} URL:${req.url} Time: ${new Date().toLocaleString()}`) //or ("Request url:",req.url,"Request method:",req.method);
+//     //res,send("Hello from middleware")  
+//     next();
+// }
+//morgan is replaceable of upeer commited code
+
+
+const apiCheckMiddleware=(req,res,next)=>{
+    if(req.query.API_KEY==="1234"){
+        console.log("Authenticated");
+        next();
+    }else{
+        res.send("API invalid");
+    }
+}
+ 
+// app.use(logMiddleware);
+app.use(apiCheckMiddleware);
+
+
+app.get("/",(req,res)=>{
+    console.log("Request name:",req.name);
+    console.log("Radhe Radhe");
+    res.send("Radhe Radhe");
+})
+
+app.get("/data",(req,res)=>{
+    console.log("Hello Data");
+    res.json({
+        city:"New York",
+        country:"USA",
+        temp:32,
+        humidity:80
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port $ {PORT}`)
+})
